3707abb337b875a954bfa7a840156af2219ec961
Beatriz Heydman <biaheydman@outlook.com>
2026-02-24 18:30:01 -0300

    fix: update error handling functionality

--- src/requests/get/get-zip/index.ts
+++ src/requests/get/get-zip/index.ts
@@ -4,13 +4,18 @@ import axios from "axios";
 // Types
 import type { Adress } from "./types";
 
-export async function zipCodeSearch(zipCode: string) {
+export async function zipCodeSearch(zipCode: string, onError: () => void) {
   try {
     const response = await axios.get<Adress>(
       `https://viacep.com.br/ws/${zipCode}/json/`,
     );
+
+    if (response.data.erro === "true") {
+      onError();
+    }
+
     return response.data;
-  } catch (error) {
-    console.error(error);
+  } catch {
+    onError();
   }
 }
